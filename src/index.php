4318f8d08c0a4725e8e5aea41f4f06859ec8519a
Beatriz Rosado <asucamedina@gmail.com>
2024-09-10 18:32:52 -0600

    Mis primeros archivos

---
+++ src/index.php
@@ -0,0 +1,7 @@
+Holi <br><br>
+Primer contenedor <br><br>
+
+<strong>Pato!</strong>
+
+<br>
+<img src="https://www.caracteristicas.co/wp-content/uploads/2017/02/pato-2-e1560917879703.jpg">
---
+++ src/pato/index.php
@@ -0,0 +1,7 @@
+Holi <br><br>
+Primer contenedor <br><br>
+
+<strong>No pato?</strong>
+
+<br>
+<img src="https://th.bing.com/th/id/OIP.QsQW7x1jLTIpyEp1t8koLwHaEc?rs=1&pid=ImgDetMain">

a9adf793e613900a8f7f28a323470cf0df6e4c35
bearozz <asucamedina@gmail.com>
2024-11-05 19:43:19 -0600

    Ahora sí funciona

--- src/index.php
+++ src/index.php
@@ -5,6 +5,7 @@ Primer contenedor <br><br>
 
 <br>
 <img src="https://www.caracteristicas.co/wp-content/uploads/2017/02/pato-2-e1560917879703.jpg">
+<h1>Pato</h1>
 
 <!-- Html -> lado del cliente -->
 <!-- Scipt -> lado del servidor -->
@@ -19,44 +20,62 @@ Primer contenedor <br><br>
 ?> -->
 
 <?php
-function loadEnv($path)
-{
-    if (!file_exists($path)) {
-        throw new Exception('El archivo .env no se encontró.');
+// function loadEnv($path)
+// {
+//     if (!file_exists($path)) {
+//         throw new Exception('El archivo .env no se encontró.');
+//     }
+
+//     $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+//     foreach ($lines as $line) {
+//         if (strpos(trim($line), '#') === 0) {
+//             continue; // Saltar los comentarios
+//         }
+
+//         list($name, $value) = explode('=', $line, 2);
+//         $name = trim($name);
+//         $value = trim($value);
+
+//         if (!array_key_exists($name, $_ENV)) {
+//             putenv("$name=$value");
+//             $_ENV[$name] = $value;
+//         }
+//     }
+// }
+
+
+// try {
+//     loadEnv('../.env');
+// } catch (Exception $e) {
+//     echo 'Error: ' . $e->getMessage();
+//     exit;
+// }
+
+// // Asignar las variables de entorno a las variables PHP
+
+// $host = $_ENV['DB_HOST'];
+// $user = $_ENV['DB_USERNAME'];
+// $password = $_ENV['DB_PASSWORD'];
+// $dbname = $_ENV['DB_NAME'];
+// $port = $_ENV['DB_PORT'];
+
+$envFile = '../.env';
+    if (file_exists($envFile)) {
+        $lines = file($envFile);
+        foreach ($lines as $line) {
+            // Ignorar comentarios y líneas vacías
+            if (trim($line) && strpos($line, '#') !== 0) {
+                putenv(trim($line));
+            }
+        }
     }
 
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) {
-            continue; // Saltar los comentarios
-        }
-
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
-
-        if (!array_key_exists($name, $_ENV)) {
-            putenv("$name=$value");
-            $_ENV[$name] = $value;
-        }
-    }
-}
-
-
-try {
-    loadEnv('../.env');
-} catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-    exit;
-}
-
-// Asignar las variables de entorno a las variables PHP
-$host = $_ENV['DB_HOST'];
-$dbname = $_ENV['DB_NAME'];
-$user = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$port = $_ENV['DB_PORT'];
-
+    // Obtener las variables de entorno
+    $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT');
+    $dbname = getenv('DB_NAME');
+    $user = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
 
 try {
     // Crear conexión usando PDO
